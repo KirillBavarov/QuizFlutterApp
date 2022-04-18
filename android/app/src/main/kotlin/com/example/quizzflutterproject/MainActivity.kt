c7aee4e40f4e15195ac8e4a0bf94cf20d5a6bd4e
k.bavarov <k.bavarov.21vek.by@gmail.com>
2022-04-18 20:07:08 +0300

    Quiz App Flutter

---
+++ android/app/src/main/kotlin/com/example/quizzflutterproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizzflutterproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/qeustion.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+
+
+class Question {
+  final String title;
+  final List<Map> answers;
+
+  Question({
+   @required this.title,
+    @required this.answers,
+});
+
+}
+
+
+class QuestionData {
+  final _data = [
+    Question(title: 'Your 18 y.o. ?',
+        answers: [
+          {'answer' : 'Yes'},
+          {'answer' : 'No'}
+        ]),
+    Question(title: 'Youe liked anime?',
+        answers: [
+          {'answer' : 'Yes'},
+          {'answer' : 'No'},
+        ]),
+    Question(title: 'how many anime have you watched?',
+        answers: [
+          {'answer' : 'less than 10 anime'},
+          {'answer' : 'less than 100 anime'},
+          {'answer' : 'over than 100 anime'},
+        ]),
+  ];
+
+  List <Question> get questions => [..._data];
+}
+
